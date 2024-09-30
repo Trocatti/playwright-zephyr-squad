259ff6bdaab67572366ce3530b0140c7a057d509
Guilherme Trocatti <28571480+Trocatti@users.noreply.github.com>
2024-09-30 16:37:35 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,11 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   reporter: [['list'], ['./src/index.ts', { 
-    host: 'https://prod-api.zephyr4jiracloud.com/connect',
-    projectKey: '14683',
-    accountId: '712020:7d8fdc76-4c6c-494d-b2f3-7eade2c7a4d7',
-    accessKey: 'amlyYTo2ZWY3M2JhMS1kNGI1LTRkMDAtYTZlMi02NTU2YWNkYWE0YzggNzEyMDIwJTNBN2Q4ZmRjNzYtNGM2Yy00OTRkLWIyZjMtN2VhZGUyYzdhNGQ3IFVTRVJfREVGQVVMVF9OQU1F',
-    secretKey: 'uAT_2FnBTvw2mvTOssQXF7oRd8dq9jggtq1p7A8ghdA',
+    host: '',
+    projectKey: '',
+    accountId: '',
+    accessKey: '',
+    secretKey: '',
   }]],
   use: {
       screenshot: 'only-on-failure'
